22404e489993a84e92072809aaa1f749d838fbee
Kristoffer Steinsland <kristoffersteins@gmail.com>
2022-10-05 16:00:57 +0200

    managed to get some samples

--- CovSampler.py
+++ CovSampler.py
@@ -14,40 +14,51 @@ T0, T1 = SE3(transforms[0]), SE3(transforms[8])
 
 #get ground truth relative transformation
 T = T0.inverse()*T1
-#c1_0 = transform_cloud(c1, T.transform())
+c1_0 = transform_cloud(c1, T.transform())
 #vis_pc([c0, c1_0])
 
-#give T some random perturbations to simulate noisy odometry
-cov_pos = 10
-cov_yaw = 0.01
-p = np.zeros(6)
-p[:2] = np.sqrt(cov_pos)*np.random.normal(size=2) # we only perturb x,y
-p[5] = np.sqrt(cov_yaw)*np.random.normal(size=1) # we only perturb yaw
-T_p = SE3Tangent(p) + T #I guess we want to add p on the left side?
+iterations = 100
+samples = []
 
-c1_0p = transform_cloud(c1, T_p.transform())
-vis_pc([c0, c1_0p])
+for i in range(iterations):
+    #give T some random perturbations to simulate noisy odometry
+    cov_pos = 1
+    cov_yaw = 0.01
+    p = np.zeros(6)
+    p[:2] = np.sqrt(cov_pos)*np.random.normal(size=2) # we only perturb x,y
+    p[5] = np.sqrt(cov_yaw)*np.random.normal(size=1) # we only perturb yaw
+    T_p = SE3Tangent(p) + T #I guess we want to add p on the left side?
 
-#convert clouds to open3d clouds
-pcd0 = o3d.geometry.PointCloud()
-pcd0.points = o3d.utility.Vector3dVector(c0)
-pcd0.estimate_normals()
+    c1_0p = transform_cloud(c1, T_p.transform())
+    #vis_pc([c0, c1_0p])
 
-pcd1 = o3d.geometry.PointCloud()
-pcd1.points = o3d.utility.Vector3dVector(c1)
-pcd0.estimate_normals()
+    #convert clouds to open3d clouds
+    pcd0 = o3d.geometry.PointCloud()
+    pcd0.points = o3d.utility.Vector3dVector(c0)
+    pcd0.estimate_normals()
 
-#do registration using open3ds point to plane icp for now
-treshold = 1
-initial_T = T_p.transform()
-reg = o3d.pipelines.registration.registration_icp(
-    pcd1, pcd0, treshold, initial_T, 
-    o3d.pipelines.registration.TransformationEstimationPointToPlane()
-)
-icp_transform = reg.transformation
-print(icp_transform)
-c1_registered = transform_cloud(c1, icp_transform)
+    pcd1 = o3d.geometry.PointCloud()
+    pcd1.points = o3d.utility.Vector3dVector(c1)
+    pcd0.estimate_normals()
 
-vis_pc([c0, c1_registered])
+    #do registration using open3ds point to plane icp for now
+    treshold = 10
+    initial_T = T_p.transform()
+    reg = o3d.pipelines.registration.registration_icp(
+        pcd1, pcd0, treshold, initial_T, 
+        o3d.pipelines.registration.TransformationEstimationPointToPlane()
+    )
+    icp_transform = reg.transformation
+    c1_registered = transform_cloud(c1, icp_transform)
+
+    samples.append(icp_transform)
+
+
+#sampled_pose_projected = np.array([[icp_transform[0,3], icp_transform[1,3], -3]])
+
+projected_samples = np.array([[samples[i][0,3], samples[i][1,3], -3] for i in range(iterations)])
+
+vis_pc([c0, c1_0, projected_samples])
+
 
 
