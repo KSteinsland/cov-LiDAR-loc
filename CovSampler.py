6d5700dffee0723f91970f0557478616a04101a0
Kristoffer Steinsland <kristoffersteins@gmail.com>
2022-11-10 16:26:12 +0100

    backup

--- CovSampler.py
+++ CovSampler.py
@@ -150,7 +150,7 @@ def add_noise_to_clouds(clouds_path, result_save_path, std_noise):
         save_path = str(result_save_path) + "/" + cloud_path.split('/')[-1]
         np.savetxt(save_path, cloud_noisy, delimiter=",")
 
-def ceate_noisy_clouds(cloud_ref, cloud_in, save_path, std_noise):
+def create_noisy_clouds(cloud_ref, cloud_in, save_path, std_noise):
     cloud = np.genfromtxt(cloud_ref, delimiter=',')
     cloud_noisy = add_noise_to_cloud(cloud, std_noise)
     ref_path = save_path / Path('./ref.csv')
@@ -168,6 +168,24 @@ def inv(T):
     T_inv[:3, :3] = T[:3, :3].T
     T_inv[:3, 3] = -T_inv[:3, :3].dot(T[:3, 3])
     return T_inv
+
+def open3d_icp(source, target, initial_T):
+    #convert clouds to open3d clouds
+    pcd0 = o3d.geometry.PointCloud()
+    pcd0.points = o3d.utility.Vector3dVector(target)
+    pcd0.estimate_normals()
+
+    pcd1 = o3d.geometry.PointCloud()
+    pcd1.points = o3d.utility.Vector3dVector(source)
+    pcd0.estimate_normals()
+
+    #do registration using open3ds point to plane icp for now
+    treshold = 0.1
+    reg = o3d.pipelines.registration.registration_icp(
+        pcd1, pcd0, treshold, initial_T, 
+        o3d.pipelines.registration.TransformationEstimationPointToPlane()
+    )
+    return reg.transformation
 
 def cov_registration_sensor_noise(cloud_dataset_path, T_gt, result_dataset_path, num_samples, std_sensor_noise_levels, overwrite=False):
     """
@@ -209,32 +227,47 @@ def cov_registration_sensor_noise(cloud_dataset_path, T_gt, result_dataset_path,
 
         clouds_path_list = sorted(glob.glob(str(cloud_dataset_path) + "/*"))
 
+        S = PCSampler()
+        S.load_samples()
+        clouds = S.get_clouds()
+
         for cld_indx in range(len(clouds_path_list)-1):
+            if cld_indx < 10: continue
             cloud_ref, cloud_in = clouds_path_list[cld_indx], clouds_path_list[cld_indx+1]
             T_gt_ref, T_gt_in = T_gt[cld_indx], T_gt[cld_indx+1]
             T_rel = T_gt_ref.inverse()*T_gt_in
+
+            c0, c1 = clouds[cld_indx], clouds[cld_indx + 1]
+            c1_0 = transform_cloud(c1, T_rel.transform())
+            vis_pc([c0, c1]) #this is correct
 
             # a place to temorarily store cloud with added noise
             working_cloud_path = result_dataset_path / Path('working_noisy_cloud')
             os.makedirs(working_cloud_path, exist_ok=True)
 
             #sample cov
-            samples = [] 
+            """ samples = [] 
             for i in range(num_samples):
-                noisy_cloud_ref, noisy_cloud_in = ceate_noisy_clouds(cloud_ref, cloud_in, working_cloud_path, noise_level)
+                noisy_cloud_ref, noisy_cloud_in = create_noisy_clouds(cloud_ref, cloud_in, working_cloud_path, noise_level)
                 icp_transform = icp_without_cov(noisy_cloud_ref, noisy_cloud_in, T_rel.transform())
                 samples.append(icp_transform)
-            mean, sampeled_cov = calc_mean_cov(samples, T_rel)
+            mean, sampeled_cov = calc_mean_cov(samples, T_rel) """
             
             #censi cov
-            noisy_cloud_ref, noisy_cloud_in = ceate_noisy_clouds(cloud_ref, cloud_in, working_cloud_path, noise_level)
-            icp_transform, censi, bonnabel = icp_with_cov(noisy_cloud_ref, noisy_cloud_in, T_rel.transform())
-            censi_cov = noise_level * censi
+            #noisy_cloud_ref, noisy_cloud_in = create_noisy_clouds(cloud_ref, cloud_in, working_cloud_path, noise_level)
+            #icp_transform, censi, bonnabel = icp_with_cov(noisy_cloud_ref, noisy_cloud_in, T_rel.transform())
+            icp_transform = icp_without_cov(cloud_ref, cloud_in, T_rel.transform())
+            #icp_transform = open3d_icp(c1, c0, T_rel.transform()) #this works
+            #censi_cov = noise_level * censi
+
+            c1_0_icp = transform_cloud(c1, icp_transform)
+            vis_pc([c0, c1_0_icp])
+
 
             #save
-            cov_result_save_path = noise_lvl_result_path / Path(f'cloud_pair_{cld_indx:03d}.p')
+            """ cov_result_save_path = noise_lvl_result_path / Path(f'cloud_pair_{cld_indx:03d}.p')
             with open(cov_result_save_path, 'wb') as f:
-                pickle.dump((censi_cov, sampeled_cov, samples, T_rel.transform()), f)
+                pickle.dump((censi_cov, sampeled_cov, samples, T_rel.transform()), f) """
 
 
 def results_reg_sens_noise(results_path):
@@ -244,6 +277,7 @@ def results_reg_sens_noise(results_path):
         if i < 9: continue
         results = sorted(glob.glob(str(noise_level_result[i]) + "/*"))
         for r in results:
+            if int(r.split('_')[-1].split('.')[0]) != 18: continue
             with open(r, 'rb') as f:
                 censi_cov, sampeled_cov, samples, T_rel = pickle.load(f)
                 sample_points2d = np.array([[s[0,3], s[1,3], 0] for s in samples])
@@ -253,7 +287,7 @@ def results_reg_sens_noise(results_path):
                 fig, ax = plt.subplots()
                 ax.plot(sample_points2d_t[:,0], sample_points2d_t[:,1], 'ro', label="samples")
                 plot_ellipse(ax, [0, 0], sampeled_cov[:2], fill_color='red', label="sample cov")
-                plot_ellipse(ax, [0, 0], censi_cov[:2], fill_color='blue', label="censi",)
+                plot_ellipse(ax, [0, 0], censi_cov[:2], fill_color='blue', label="censi")
                 plt.legend(loc="best")
                 plt.title(f"Sensor noise standard deviation: {lvl}")
                 #plt.savefig(f'./sample_and_censi_v_noise/clouds{i}and{i+1}_noise_{std_sensor_noise}.png')
@@ -281,8 +315,8 @@ if __name__ == "__main__":
     numb_mc_samples = 100
     std_sensor_noise_levels = [n/1000 for n in range(11)]
     
-    #cov_registration_sensor_noise(dataset_clouds_path, transforms_se3, results_path, numb_mc_samples, std_sensor_noise_levels)
-    results_reg_sens_noise(results_path)
+    cov_registration_sensor_noise(dataset_clouds_path, transforms_se3, results_path, numb_mc_samples, std_sensor_noise_levels, overwrite=True)
+    #results_reg_sens_noise(results_path)
 
 
 
